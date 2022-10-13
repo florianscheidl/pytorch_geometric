eb0603a2a08795aacc943f3f2d9e1c65aa87ded8
florian.scheidl@gmail.com <florian.scheidl@gmail.com>
2022-10-13 17:19:25 +0200

    Amendments on the module structure (import sys path)

--- graphgym/run2.sh
+++ graphgym/run2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Test for running a single experiment. --repeat means run how many different random seeds.
-python liwich_main.py --cfg configs/hetero_mutag_heteroconv.yaml --repeat 3 # node classification
+python graphgym/liwich_main.py --cfg graphgym/configs/hetero_mutag_heteroconv.yaml --repeat 3 # node classification
