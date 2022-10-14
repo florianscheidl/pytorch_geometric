715074a382bda8312cdbefcf7b930999494ef9ca
florian.scheidl@gmail.com <florian.scheidl@gmail.com>
2022-10-14 15:14:33 +0200

    Sweep and config adjustments

--- graphgym/liwich_main.py
+++ graphgym/liwich_main.py
@@ -73,8 +73,8 @@ if __name__ == '__main__':
         wandb.init(config=cfg)
     logging.info(model)
     logging.info(cfg)
-    dummy_batch = transformed_dataset.data
+    dummy_batch = transformed_dataset.data.to(cfg.accelerator)
     model(dummy_batch) # lazy initialisation, sometimes this seems to be necessary, not always though
     cfg.params = params_count(model)  # -> would need to initialize lazy modules.
     logging.info('Num parameters: %s', cfg.params)
-    wandb_train.train(model, datamodule, logger=True, use_wandb=use_wandb)
+    wandb_train.tracuin(model, datamodule, logger=True, use_wandb=use_wandb)
